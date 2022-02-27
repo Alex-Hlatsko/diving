d00e7fcc9dba3572a0ef3d792c43f9a591223971
Alen-Berg <alexei.hlatsko@gmail.com>
2022-02-27 19:17:53 +0100

    basic structure

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+//Imports
+import gulp from 'gulp';
+import { path } from './gulp/config/path.js';
+import { plugins } from './gulp/config/plugins.js';
+
+//Import tasks
+import { styles } from './gulp/tasks/styles.js';
+import { scripts } from './gulp/tasks/scripts.js';
+import { html } from './gulp/tasks/html.js';
+import { server } from './gulp/tasks/server.js';
+import { images } from './gulp/tasks/images.js';
+import { reset } from './gulp/tasks/reset.js';
+import { fonts } from './gulp/tasks/fonts.js';
+
+//Global variable
+global.app = {
+    isBuild: process.argv.includes('--build'),
+    isDev: !process.argv.includes('--build'),
+    path: path,
+    gulp: gulp,
+    plugins: plugins
+}
+
+//Monitoring files
+function watcher(){
+    gulp.watch(app.path.watch.styles, gulp.series(reset, styles))
+    gulp.watch(app.path.watch.scripts, gulp.series(reset, scripts))
+    gulp.watch(app.path.watch.html, gulp.series(reset, html))
+    gulp.watch(app.path.watch.images, gulp.series(reset, images))
+}
+
+//General tasks
+const mainTasks = gulp.parallel(html, styles, scripts, images, fonts);
+
+//Modes
+const dev = gulp.series(reset, mainTasks, gulp.parallel(server, watcher));
+const build = gulp.series(reset, mainTasks);
+
+//Export mods
+export { dev }
+export { build }
+gulp.task('default', dev)
